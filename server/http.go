2bc602a95fef7c860d983549369c2a936e7b53b1
水谷健太 <k.mizutani0222@gmail.com>
2016-03-15 14:38:31 +0900

    サーバーのコード

---
+++ server/http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "net/http"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    param1 := req.FormValue("param1")
+    param2 := req.FormValue("param2")
+    output := `{"header":{"code":200,"message":"OK"},"body":{"param1":` + html.EscapeString(param1) + `, "param2:"` + html.EscapeString(param2) + `}`
+    fmt.Fprintf(w, "%s", output)
+}
+
+func main() {
+    http.HandleFunc("/apisample", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
