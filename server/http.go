c8467af2cf89c5f0fea50072f3d42a7a54e14729
水谷健太 <k.mizutani0222@gmail.com>
2016-03-15 14:48:42 +0900

    レスポンスの修正

--- server/http.go
+++ server/http.go
@@ -9,7 +9,7 @@ import (
 func HelloServer(w http.ResponseWriter, req *http.Request) {
     param1 := req.FormValue("param1")
     param2 := req.FormValue("param2")
-    output := `{"header":{"code":200,"message":"OK"},"body":{"param1":` + html.EscapeString(param1) + `, "param2:"` + html.EscapeString(param2) + `}`
+    output := `{"header":{"code":200,"message":"OK"},"body":{"param1":"` + html.EscapeString(param1) + `", "param2":"` + html.EscapeString(param2) + `"}`
     fmt.Fprintf(w, "%s", output)
 }
 
